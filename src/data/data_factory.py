a18dde5aebc065f7f675b3f3c39743a4b5d5ee9b
tunlaton11 <tunlaton11@gmail.com>
2023-12-01 02:09:50 +0700

    exclude argoverse dataset

--- src/data/data_factory.py
+++ src/data/data_factory.py
@@ -6,9 +6,9 @@ from nuscenes import NuScenes
 from .nuscenes.dataset import NuScenesMapDataset
 from .nuscenes.splits import TRAIN_SCENES, VAL_SCENES, CALIBRATION_SCENES
 
-from argoverse.data_loading.argoverse_tracking_loader import ArgoverseTrackingLoader
-from .argoverse.dataset import ArgoverseMapDataset
-from .argoverse.splits import TRAIN_LOGS, VAL_LOGS
+# from argoverse.data_loading.argoverse_tracking_loader import ArgoverseTrackingLoader
+# from .argoverse.dataset import ArgoverseMapDataset
+# from .argoverse.splits import TRAIN_LOGS, VAL_LOGS
 
 
 def build_nuscenes_datasets(config):
@@ -29,29 +29,29 @@ def build_nuscenes_datasets(config):
     return train_data, val_data
 
 
-def build_argoverse_datasets(config):
-    print('==> Loading Argoverse dataset...')
-    dataroot = os.path.expandvars(config.dataroot)
+# def build_argoverse_datasets(config):
+#     print('==> Loading Argoverse dataset...')
+#     dataroot = os.path.expandvars(config.dataroot)
     
-    # Load native argoverse splits
-    loaders = {
-        'train' : ArgoverseTrackingLoader(os.path.join(dataroot, 'train')),
-        'val' : ArgoverseTrackingLoader(os.path.join(dataroot, 'val'))
-    }
+#     # Load native argoverse splits
+#     loaders = {
+#         'train' : ArgoverseTrackingLoader(os.path.join(dataroot, 'train')),
+#         'val' : ArgoverseTrackingLoader(os.path.join(dataroot, 'val'))
+#     }
 
-    # Create datasets using new argoverse splits
-    train_data = ArgoverseMapDataset(loaders, config.label_root, 
-                                     config.img_size, TRAIN_LOGS)
-    val_data = ArgoverseMapDataset(loaders, config.label_root, 
-                                   config.img_size, VAL_LOGS)
-    return train_data, val_data
+#     # Create datasets using new argoverse splits
+#     train_data = ArgoverseMapDataset(loaders, config.label_root, 
+#                                      config.img_size, TRAIN_LOGS)
+#     val_data = ArgoverseMapDataset(loaders, config.label_root, 
+#                                    config.img_size, VAL_LOGS)
+#     return train_data, val_data
 
 
 def build_datasets(dataset_name, config):
     if dataset_name == 'nuscenes':
         return build_nuscenes_datasets(config)
-    elif dataset_name == 'argoverse':
-        return build_argoverse_datasets(config)
+    # elif dataset_name == 'argoverse':
+    #     return build_argoverse_datasets(config)
     else:
         raise ValueError(f"Unknown dataset option '{dataset_name}'")
 
